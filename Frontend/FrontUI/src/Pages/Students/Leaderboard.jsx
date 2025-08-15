a3e8b92513634f732f0a26fb5570433abd1d43fe
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-08-15 20:00:30 +0500

    Users LeaderBoard

--- Frontend/FrontUI/src/Pages/Students/Leaderboard.jsx
+++ Frontend/FrontUI/src/Pages/Students/Leaderboard.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import AxiosInstance from '../../Utility/AxiosInstance'
 import { API_PATHS } from '../../Utility/API_Path'
-import Award from "./assests/Award.svg"
 import { Trophy, TrophyIcon } from 'lucide-react'
+import LeaderBoardHeader from './Components/LeaderBoardHeader'
+import ChallengeCard from '../../Components/Cards/User/ChallengeCard '
 const Leaderboard = () => {
   const [TopPerformers, setTopPerformers] = useState([])
-  console.log("TopPerformers",TopPerformers);
+  const [LeaderBoardData, setLeaderBoardData] = useState([])
+  console.log("LeaderBoardData",LeaderBoardData);
   
   const FetchTopPerformer = async()=>{
     try {
@@ -21,63 +23,51 @@ const Leaderboard = () => {
     }
   }
 
+  const FetchCompletedChallenges = async()=>{
+    try {
+      const result = await AxiosInstance.get(API_PATHS.CHALLENGE.GET_LEADERBOARD);
+      if(result.data)
+      {
+        setLeaderBoardData(result.data)
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   useEffect(()=>{
     FetchTopPerformer();
   },[])
+  useEffect(() => {
+    FetchCompletedChallenges();
+  }, [])
+  
 
   return (
     <div className='font-urbanist min-h-screen  px-4 '>
-      <div className='relative grid grid-cols-3 border mt-8 rounded-xl p-4 text-black bg-purple-200 border-purple-400  min-h-64 overflow-hidden'>
-       
-
-        <div className='col-span-1 relative '>
-            <div className='absolute translate-x-40 text-purple-600 translate-y-10 '>
-              <TrophyIcon className='size-28'/>
-            {/* <img src={Award} className='size-64 ' alt="" /> */}
-        </div>
-
-        </div>
-        <div className="col-span-1 text-lg space-y-2 font-medium text-black -translate-y-7  line-clamp-2 flex flex-col items-center justify-center ">
-          <h1 className='text-2xl'>Code Ascend</h1>
-          <p>Coding Competition • Live Rankings</p>
-          <p className='text-xs'>Where the Best Rise to the Top</p>
-        </div>
-
-        <div className='col-span-1 translate-y-6  mr-3'>
-          {
-            TopPerformers.map((item, index) => (
-            <div 
-              key={item._id._id || index} 
-              className="flex items-center justify-between text-[16px] font-medium border-b border-purple-400 w-full space-y-4 px-4"
-            >
-              <div className="flex items-center space-x-4 ">
-                <span className={`rounded-lg size-9  flex items-center justify-center ${index+1 == 1 ? "bg-[#FFD700]": index+1 == 2 ?"bg-[#C0C0C0]":"bg-[#CD7F32]"}`}>
-                  <div className='text-lg'>{index + 1}</div>
-                </span>
-                <h1 className="">{item._id.name}</h1>
-                
-              </div>
-
-              <div>
-                <h1 className=" mb-4">
-                  {item.submissionCount * 987} points
-                </h1>
-              </div>
+      <LeaderBoardHeader TopPerformers={TopPerformers} />
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+              {
+                LeaderBoardData.map((item)=>{
+                return ( 
+                  <ChallengeCard
+                      tag={"Leaderboard"}
+                      ID={item._id}
+                      title = {item.title}
+                      description = {item.description}
+                      priority= {item.difficulty}  
+                      startTime={item.startTime}
+                      endTime={item.endTime}
+                      status ={item.duration}
+                      dueDate = {item.dueDate || ""}
+                      onselect={(ID)=>ConfirmID(ID)}
+                    
+                  />
+                )
+                })
+              }
             </div>
-          ))
-
-          }
-        </div>
-        <div className='absolute  bottom-0 z-20 '>
-            <svg className='h-32 w-full overflow-hidden' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#c084fc " fill-opacity="1" d="M0,224L288,64L576,160L864,32L1152,224L1440,320L1440,320L1152,320L864,320L576,320L288,320L0,320Z"></path></svg>
-        </div>
-        <div className='absolute  bottom-0 '>
-            <svg className='h-32 translate-x-2 w-full overflow-hidden' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#9333ea" fill-opacity="1" d="M0,224L288,64L576,160L864,32L1152,224L1440,320L1440,320L1152,320L864,320L576,320L288,320L0,320Z"></path></svg>
-        </div>
-        
-      </div>
-      
-
 
     </div>
   )
